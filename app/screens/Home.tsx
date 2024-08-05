ef8e684f94d760a33f7d67f7183c9ae971c6f3ba
maciej.lodygowski <maciej.lodygowski@callstack.com>
2024-08-05 16:56:21 +0200

    Add search

--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,32 +1,36 @@
-import {
-  ActivityIndicator,
-  FlatList,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-import React from 'react';
+import {ActivityIndicator, FlatList, StyleSheet, View} from 'react-native';
+import React, {useMemo, useState} from 'react';
 import {useLoadForecast} from '../hooks/useLoadForecast.ts';
 import {WeatherListItem} from '../components/WeatherListItem.tsx';
 import {useNavigation} from '@react-navigation/native';
 import {AppStackNavigationProps} from '../AppRouter.tsx';
 import {ErrorMessage} from '../components/ErrorMessage.tsx';
 import {useCities} from '../hooks/useCities.ts';
+import {SearchBar} from '../components/SearchBar.tsx';
 
 export const HomeScreen = () => {
   const cities = useCities();
   const forecast = useLoadForecast(cities.data);
   const navigation = useNavigation<AppStackNavigationProps>();
+  const [search, setSearch] = useState('');
+  const filteredForecast = useMemo(
+    () =>
+      forecast.data?.data.list.filter(item =>
+        item.name.toLowerCase().includes(search.toLowerCase()),
+      ),
+    [forecast.data, search],
+  );
 
   return (
     <View style={[styles.container]}>
+      <SearchBar onChange={setSearch} />
       {forecast.isLoading && <ActivityIndicator />}
       {forecast.isSuccess && (
         <FlatList
           refreshing={forecast.isRefetching}
           onRefresh={forecast.refetch}
           style={[styles.container]}
-          data={forecast.data?.data.list}
+          data={filteredForecast}
           renderItem={({item}) => (
             <WeatherListItem
               onPress={() => navigation.navigate('Details', item)}
