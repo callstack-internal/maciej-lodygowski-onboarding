ee2f68c4ccf8ffca8477466df24e35897d387382
maciej.lodygowski <maciej.lodygowski@callstack.com>
2024-10-16 21:07:30 +0200

    Fix perf test

--- performance/.perf-test.tsx
+++ performance/.perf-test.tsx
@@ -1,11 +1,12 @@
 import {measureRenders} from 'reassure';
-import {HomeScreen} from '../app/screens/Home.tsx';
 import {describe, test} from '@jest/globals';
 import {NavigationContainer} from '@react-navigation/native';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {setupAPIMocks} from '../__tests__/helpers/mock-api.ts';
 import {loadForecastHandler} from '../mocks/handlers.ts';
 import {fireEvent, screen} from '@testing-library/react-native';
+import {AppRouter} from '../app/AppRouter.tsx';
+import {AuthContext, AuthProvider} from '../app/context/AuthContext.tsx';
 
 describe('Performance tests', () => {
   const mocks = setupAPIMocks([loadForecastHandler]);
@@ -22,14 +23,22 @@ describe('Performance tests', () => {
         },
       },
     });
+    const loginStub = () => {};
 
     await measureRenders(
       <NavigationContainer>
-        <QueryClientProvider client={client}>
-          <HomeScreen />
-        </QueryClientProvider>
+        <AuthContext.Provider
+          value={{
+            isLoggedIn: true,
+            setIsLoggedIn: loginStub,
+            setLogout: loginStub,
+          }}>
+          <QueryClientProvider client={client}>
+            <AppRouter />
+          </QueryClientProvider>
+        </AuthContext.Provider>
       </NavigationContainer>,
       {scenario},
     );
-  });
+  }, 10000);
 });
